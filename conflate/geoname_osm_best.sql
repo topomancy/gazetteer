5fccca257ad50626c6159916302c8a258d7d3cd6
Schuyler Erle <schuyler@nocat.net>
2012-08-27 21:30:39 +0200

    Finish moving stuff around. (for now.)

--- conflate/geoname_osm_best.sql
+++
@@ -1,4 +0,0 @@
-create table geoname_osm_best as
-select distinct * from geoname_osm_match a where a.osmid=(select
-osmid from geoname_osm_match b where a.geonameid = b.geonameid order by
-similarity desc limit 1);
--- conflate/geonames_alt.sql
+++
@@ -1,9 +0,0 @@
-create table alt_names (
-    altnameid integer,
-    geonameid integer,
-    lang varchar(7),
-    alt_name varchar,
-    is_preferred char(1),
-    is_short char(1)
-);
-
