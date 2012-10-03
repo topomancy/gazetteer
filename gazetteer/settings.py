d6c5acea5308c5861bf975ba88b5f5cbf1e1dc74
Sanjay B <b@pad.ma>
2012-10-04 01:07:16 +0530

    Moved old project to prototype folder Created empty new django 1.4 project

--- gazetteer/settings.py
+++ gazetteer/settings.py
@@ -1,27 +1,19 @@
 # Django settings for gazetteer project.
-import os
-from os.path import join
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-JSON_DEBUG = DEBUG
-# DATA_DIR = '/home/sanj/c/gazetteer/data'
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    # ('Your Name', 'your_email@example.com'),
 )
-INTERNAL_IPS = ('127.0.0.1',)
+
 MANAGERS = ADMINS
-
-LOCAL_DEVELOPMENT = True
-
-PROJECT_ROOT = os.path.dirname(__file__)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'gazzette',                      # Or path to database file if using sqlite3.
-        'USER': 'sanj',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -48,28 +40,48 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
 
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = join(PROJECT_ROOT, 'media')
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
 
-STATIC_ROOT = join(PROJECT_ROOT, 'static')
-
-STATIC_URL = '/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-#ADMIN_MEDIA_PREFIX = '/static/'
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '4a$-vh!9+^)rzhvq%uq)!qq)!4th=315o-q@-$2g81n0%(&c@a'
+SECRET_KEY = 'yc968agrkgxt@_((mmuyx3dt_at_=^=k2dr88^^8b8&amp;#r*%n4i'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -84,38 +96,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
-    "places.context_processors.gazetteer_params",
-)
-
-AJAX_LOOKUP_CHANNELS = {
-    # the simplest case, pass a DICT with the model and field to search against :
-    'authority_record' : dict(model='places.authorityrecord', search_field='preferred_name'),
-    'time_frame': dict(model='places.timeframe', search_field='description'),
-    'feature_type': ('places.lookups', 'FeatureTypeLookup'),
-    'feature': dict(model='places.feature', search_field='preferred_name'),
-    # this generates a simple channel
-    # specifying the model Track in the music app, and searching against the 'title' field
-
-    # or write a custom search channel and specify that using a TUPLE
-#    'contact' : ('peoplez.lookups', 'ContactLookup'),
-    # this specifies to look for the class `ContactLookup` in the `peoplez.lookups` module
-}
 
 ROOT_URLCONF = 'gazetteer.urls'
 
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'gazetteer.wsgi.application'
+
 TEMPLATE_DIRS = (
-    join(PROJECT_ROOT, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -127,26 +117,38 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.gis',
     'django.contrib.staticfiles',
-    'places',
-    'django_extensions',
-    'debug_toolbar',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-#    'django.contrib.admindocs',
-    'ajax_select',
+    # 'django.contrib.admindocs',
 )
 
-#Over-ride these in local_settings.py
-GAZETTEER_PARAMS = {
-    'supported_by_website_url': 'http://www.loc.gov/index.html',
-    'supported_by_image_url': '/static/images/logo-loc.png',
-#    'supported_by_name': 'Library of Congress'        
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
 }
-
-try:
-  from local_settings import *
-except:
-  pass
--- gazetteer/urls.py
+++ gazetteer/urls.py
@@ -1,35 +1,17 @@
-from django.conf.urls.defaults import *
-from os.path import join
-import settings
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^gazetteer/', include('gazetteer.foo.urls')),
-    (r'^$', 'places.views.search'),
-    ('^search/?$', 'places.views.search'),
-    ('^feature/search.json$', 'places.views.search_json'),
-    ('^feature/(?P<id>\d+).json$', 'places.views.feature_json'),
-    ('^feature/(?P<id>\d+)/similar.json$', 'places.views.search_related_json'),
-    ('^auth_record.json$', 'places.views.auth_record_json'),
-    ('^time_frame.json$', 'places.views.time_frame_json'),
-    ('^add_relation$', 'places.views.add_relation'),
+    # Examples:
+    # url(r'^$', 'gazetteer.views.home', name='home'),
+    # url(r'^gazetteer/', include('gazetteer.foo.urls')),
+
     # Uncomment the admin/doc line below to enable admin documentation:
-#    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^ajax_select/', include('ajax_select.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-
+    # url(r'^admin/', include(admin.site.urls)),
 )
-
-#if settings.LOCAL_DEVELOPMENT:
-##
-#  urlpatterns += patterns('',
-##
-#  (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': join(settings.PROJECT_ROOT, "static")}),
-##
-#)
-
