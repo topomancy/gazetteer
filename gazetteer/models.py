58f860a453761a4e62643fd29d21468f997baa78
Tim Waters <chippy2005@gmail.com>
2012-10-10 17:17:33 +0100

    Add simple ES search in dummy model and add instructions for installing the es libraries and extra django stuff to readme

--- gazetteer/models.py
+++ gazetteer/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from pyelasticobjects import *
+import json
 
 class Dummy(models.Model):
     question = models.CharField(max_length=200)
+    
+    def es_search(self):
+        conn = ObjectSearch('http://localhost:9200/')        
+        conn.index("dummy-index", "user", {"user": "Joe Tester"}, 1)
+        docs = conn.search("user:Tester")
+        print docs[0].source
+        conn.delete_index("dummy-index")
