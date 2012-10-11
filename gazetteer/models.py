97834c4b71ec06df23066e2247a4b49e37828224
Tim Waters <chippy2005@gmail.com>
2012-10-11 15:57:41 +0100

    Add separate ESobject powered place model, and link it from dummy db model

--- gazetteer/models.py
+++ gazetteer/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from pyelasticobjects import *
 import json
 
+from place import *
+
 class Dummy(models.Model):
     question = models.CharField(max_length=200)
     
-    def es_search(self):
+    def get_place(self, name):
+        p = Place()
+        return p.get_name(name)
+        
+    
+    @classmethod
+    def es_search(klass):
         conn = ObjectSearch('http://localhost:9200/')        
         conn.index("dummy-index", "user", {"user": "Joe Tester"}, 1)
+        conn.refresh(["dummy-index"])
         docs = conn.search("user:Tester")
+        
         print docs[0].source
         conn.delete_index("dummy-index")
+        
---
+++ gazetteer/place.py
@@ -0,0 +1,22 @@
+from pyelasticobjects import *
+import json
+
+class Place():
+    
+    
+    def get_name(self, name):
+        conn = ObjectSearch('http://localhost:9200/')
+        docs = conn.search("name:"+ name)
+        if docs[0].source:
+            return docs[0].source
+        return None
+        
+    def set_name(self, name):
+        conn = ObjectSearch('http://localhost:9200/')
+        conn.index("dummy-index", "user", {"name": name}, 1)
+        conn.refresh(["dummy-index"])
+        
+    
+    def count(self, n):
+        conn = ObjectSearch('http://localhost:9200/')
+        return conn.count("*")
