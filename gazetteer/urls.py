67b668adf9bb8fa47f4c666373b30910136c9dc9
Sanjay B <b@pad.ma>
2012-10-18 00:21:29 +0530

    add /1.0/ prefix for api

--- gazetteer/urls.py
+++ gazetteer/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'gazetteer.views.index'), 
     url(r'^search/', 'gazetteer.views.search'),
-    url(r'^place/', include(api_urls)),
+    url(r'^1.0/place/', include(api_urls)),
 )
 
 
