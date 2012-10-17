cd12dfddc5c9e6b5042ef95c6aad570b784a8b2f
Tim Waters <chippy2005@gmail.com>
2012-10-17 15:51:59 +0100

    clean up place model a bit more, make it more django friendly, remove class level methods

--- gazetteer/place.py
+++ gazetteer/place.py
@@ -1,13 +1,12 @@
 from pyelasticobjects import *
 import json
 
-#INDEX="gazetteer"
-#TYPE="place"
-
 class PlaceManager:
 
-    def __init__(self, conn=None):
+    def __init__(self, conn, index, es_type):
         self.conn = conn
+        self.index = index
+        self.es_type = es_type
 
     #counts number of times an object with the specified keyword somewhere included
     #Place.objects.name_count("dam")
@@ -24,31 +23,58 @@ class PlaceManager:
     #gets the specified place as a Place object
     #Place.objects.get("0908d08a995ab874")
     def get(self, obj_id):
-        return self.conn.get("gazetteer", "place", obj_id) 
+        return self.conn.get(self.index, self.es_type, obj_id) 
 
-class Place:
-
-    conn = ObjectSearch('http://localhost:9200/') 
-    objects = PlaceManager(conn)
-
-    #gets json document from this place, by passing in the Place object itself
-    #or by passing in the id of the document
-    @classmethod
-    def extract_document(obj_id, obj=None):
+    #returns similar objects
+    def find_similar(self, place):
         return None
 
-    #creates index with specified index name and tye    
-    @classmethod
-    def create_index(index_name, es_type):
+    #returns the heirarchy of this object
+    def heirarchy(self, place):
         return None
 
-    #Adds record to ES index
-    @classmethod
-    def index():
+
+    #Adds a new place record to elastic search backend
+    def add(self, place):
+        return None
+
+
+    #ElasticSearch index methods
+
+    #adds an index (new record) directly to ES via json
+    def index(self, place_json):
+        return None
+
+    #creates index with mapping    
+    def create_mapping(self, mapping_json):
         return None
 
     #refreshes the ES index
-    @classmethod
     def refresh_index():
         return None
+
+class Place:
+    
+    #TODO - get these strings from a config file.
+    conn = ObjectSearch('http://localhost:9200/') 
+    objects = PlaceManager(conn, "gazetteer", "place")  
+
+    #creates a new Place object 
+    def __init__(self):
+        return None
+
+    #saves the new / changed object
+    def save(self):
+        #calls Place.objects.save(self) or something?
+        return None
+
+    #gets json document from this place
+    def to_json(self):
+        return None
+
+    def find_similar(self):
+        #call Place.objects.find_similar(self)
+        return None
+
+    
 
