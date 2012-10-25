9106b250bde6ede581a45298fb77264606b886dc
Sanjay B <b@pad.ma>
2012-10-26 04:07:32 +0530

    add model for FeatureCode, basic csv import

--- gazetteer/models.py
+++ gazetteer/models.py
@@ -1,2 +1,22 @@
 from django.db import models
+import csv
 
+class FeatureCode(models.Model):
+    cls = models.CharField(max_length=3, blank=True)
+    typ = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=64, blank=True)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.typ
+
+    @classmethod
+    def import_from_csv(kls, path):
+        CsvFile = csv.reader(open(path), delimiter="\t")
+        for row in CsvFile:
+            cls, typ = row[0].split(".")
+            fcode = kls(cls=cls, typ=typ, name=row[1], description=row[2])
+            fcode.save()
+            print "saved " + row[1]
+
+# Create your models here.
