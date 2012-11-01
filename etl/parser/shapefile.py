b78d3b626aa06fbe9b8dc5df8510a3d53586bb34
Tim Waters <chippy2005@gmail.com>
2012-11-01 18:37:47 -0400

    Add better working fiona powered building shapefile ETL importer

--- etl/parser/shapefile.py
+++ etl/parser/shapefile.py
@@ -1,85 +1,83 @@
 # pass in shapefile 
 #open shapefile
 import sys
-import ogr
+from shapely.geometry import asShape
 from fiona import collection
 
-dataset = ogr.Open( "/home/tim/projects/gaz/queens_buildings/buildings/buildings.shp" )
-#buildings
-#id origin + shapefile_name + id 
-#['user_id', 761] (source > properties
-#['layer_id', 870] --- (source > properties
-#['created_at', '2012/10/23']
-#['updated_at', '2012/10/23'] -- updated  (date)
-#['comment', None]  (source > properties > comment)
-#['name', None]  -- (source > properties > name)
-#['number', None] (source > properties > number)
-#['street', 'Fresh Meadow Road'] (source > properties > street)
-#['materials', 'brick'] (source > properties > material)
-#['use_type', None] -- feature_code (source > properties > use_type)
-#['use_subt82', None] (source > properties > use_subtype)
-#['layer_year', '1909']  -- timeframe
 
-#place = {
-    #"name": geoname["name"],
-    #"centroid": centroid,
-    #"feature_code": geoname["feature_code"],
-    #"geometry": {"type": "Point", "coordinates": centroid},
-    #"is_primary": True,
-    #"source": geoname,
-    #"alternate": names,
-    #"updated": geoname["changed_at"],
-    #"population": population,
-    #"uris": [uri],
-    #"relationships": [],
-    #"timeframe": {},
-    #"admin": []
-#}
+for feature in collection("/home/tim/projects/gaz/queens_buildings/buildings/buildings.shp", "r"):
     
-lyr = dataset.GetLayer()
-lyr.ResetReading()
-feat_defn = lyr.GetLayerDefn()
-print feat_defn
-count = 0
-for feature in lyr:
-    count = count + 1
-    #uri = "http://maps.nypl.org/digitizer/queens_buildings_bromley_1909_" + count 
-    feat_defn = lyr.GetLayerDefn()
-    name = ""
-    for i in range(feat_defn.GetFieldCount()):
-        field_defn = feat_defn.GetFieldDefn(i)
-        field_name = field_defn.GetName()
-        field_value = feature.GetField(i)
-        #print [field_name, field_value]
-        #print feature.GetFieldIndex("name")
-    updated_at = feature.GetField(feature.GetFieldIndex("updated_at"))
+    geometry = feature["geometry"]
+    properties = feature["properties"]
     
-    name = feature.GetField(feature.GetFieldIndex("name"))
-    if name:
-        print name
-        print updated_at
+    #Set name.
+    #If a building has no name, give it Number and Street Address.
+    number = properties["number"]
+    street = ""
+    if number:
+        street = " "
+    street = street + properties["street"]
+    name = number + street 
         
+    if properties["name"]:
+        name = properties["name"]
     
-    geom = feature.GetGeometryRef()
-    centroid = [geom.Centroid().GetX(), geom.Centroid().GetY()]
-    #print centroid #geom.Centroid().ExportToWkt()
+    centroid = [asShape(geometry).centroid.x , asShape(geometry).centroid.y]
     
-    geometry = geom.ExportToJson()
-    #print geometry
+    use_types = {
+        "Residential" : "HSE",
+        "Worship" : "CTTR",
+        "Educational" : "SCH",
+        "Commercial": "BLDO",
+        "Industrial" : "MFG",
+        "Health": "HSP",
+        "Transport":"BLDG",
+        "Military":"INSM",
+        "unknown": "BLDG"
+    }
+    use_sub_types = {
+            "Apartments": "HSE",
+            "Houses": "HSE",
+            "Church" : "CH",
+            "Synagogue" : "CTRR",
+            "School": "SCH",
+            "Railroad System" : "RSTN",
+            "Bank" : "BANK",
+            "Hotel":"HTL",
+            "Library": "LIBR",
+            "Freight House":"RSTN",
+            "Hospital" : "HSP",
+            "Lumber Yard":"MLSW"
+    }
+    #
+    # SEE https://github.com/timwaters/mapwarper/blob/master/app/controllers/digitize_controller.rb
+    # For all the types
+    # Currently will break with a KeyError if the mapping is not there.
+    feature_code = "BLDG"
+    if properties["use_type"]:
+        feature_code = use_types[properties["use_type"]]
+    if properties["use_subt82"]:
+        feature_code = use_sub_types[properties["use_subt82"]]
     
+    source = properties
+    uri = "/nypl/warper/layer/1234/queens_buildings/buildings.shp,"+ feature["id"]
+    timeframe = {"start_date": properties["layer_year"], "end_date": properties["layer_year"]}
+    
+    place = {
+        "name":name,
+        "centroid":centroid,
+        "feature_code": feature_code,
+        "geometry":geometry,
+        "is_primary": True,
+        "source": source,
+        "uris":[uri],
+        "relationships": [],
+        "timeframe":timeframe,
+        "admin":[]
+        
+    }
+    print place
 
-
-ds = None
-
-#for feat in layer:
-    #print feat
-
-#get geometry
-
-#get mapping
-
-#shapely to get centroid
-
-#shapely to convert to geojson
 
 #export to es index 
+
