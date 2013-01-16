afcd6083bb1e565ddde44c843bb352f5eb31ef25
Sanjay B <b@pad.ma>
2013-01-16 16:45:12 +0530

    add custom user model

--- gazetteer/models.py
+++ gazetteer/models.py
@@ -1,5 +1,65 @@
 from django.db import models
 import csv
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+ 
+        user = self.model(
+            email=UserManager.normalize_email(email),
+        )
+ 
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+ 
+    def create_superuser(self, email, password):
+        user = self.create_user(email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True, db_index=True)
+ 
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+ 
+    objects = UserManager()
+ 
+    USERNAME_FIELD = 'email'
+ 
+    def get_full_name(self):
+        # For this case we return email. Could also be User.first_name User.last_name if you have these fields
+        return self.email
+ 
+    def get_short_name(self):
+        # For this case we return email. Could also be User.first_name if you have this field
+        return self.email
+ 
+    def __unicode__(self):
+        return self.email
+ 
+    def has_perm(self, perm, obj=None):
+        # Handle whether the user has a specific permission?"
+        return True
+ 
+    def has_module_perms(self, app_label):
+        # Handle whether the user has permissions to view the app `app_label`?"
+        return True
+ 
+    @property
+    def is_staff(self):
+        # Handle whether the user is a member of staff?"
+        return self.is_admin
+
 
 class FeatureCode(models.Model):
     cls = models.CharField(max_length=3, blank=True)
@@ -19,7 +79,6 @@ class FeatureCode(models.Model):
             'description': self.description
         }
 
-
     @classmethod
     def import_from_csv(kls, path):
         CsvFile = csv.reader(open(path), delimiter="\t")
@@ -29,4 +88,6 @@ class FeatureCode(models.Model):
             fcode.save()
             print "saved " + row[1]
 
-# Create your models here.
+    
+
+    
--- gazetteer/settings.py
+++ gazetteer/settings.py
@@ -14,6 +14,8 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+AUTH_USER_MODEL = 'gazetteer.User'
+
 INTERNAL_IPS = ('127.0.0.1',)
 
 MANAGERS = ADMINS
