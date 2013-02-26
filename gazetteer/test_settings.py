2010baba087fb66532a586779c18e1d84e7c90d4
Tim Waters <chippy2005@gmail.com>
2013-02-26 13:24:41 +0000

    Fix for bug where similar places was not working for place with no geo. closes #166

--- gazetteer/test_settings.py
+++ gazetteer/test_settings.py
@@ -3,6 +3,7 @@ from settings import *
 #
 # Settings file for use with the tests, so that they use sqlite and a custom ES index
 #
-
-DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+#TODO - look into using spatialite if that speeds things up?
+#DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+DATABASES['default'] = {'ENGINE': 'django.contrib.gis.db.backends.postgis', 'NAME':'gazdev'}
 ELASTICSEARCH["default"]["INDEX"] = "gaz-test-index"
