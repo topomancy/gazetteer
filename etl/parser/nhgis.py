a97b76e7f85c2870ee29e13e11fa8f4f07bbbf6e
Tim <tim@topomancy.(none)>
2013-03-20 17:49:44 +0100

    Change NHGIS parser to use new merged dataset

--- etl/parser/nhgis.py
+++ etl/parser/nhgis.py
@@ -9,7 +9,7 @@ from core import Dump
 # Opens a directory containing all the shapefiles, and extracts
 #
 
-def extract_shapefile(shapefile, uri_name, simplify_tolerance, country_code, geo_type, year):
+def extract_shapefile(shapefile, uri_name, simplify_tolerance):
     #with collection(shapefile, 'r') as source:
     #  #print country_code, geo_type, year, os.path.basename(shapefile), source.schema["properties"].keys()
     
@@ -27,31 +27,35 @@ def extract_shapefile(shapefile, uri_name, simplify_tolerance, country_code, geo
         except AttributeError:
             print "Error: ", feature
             continue
-            
+        
+	admin = []    
         alternates = []
         name = None
         feature_code = "ADM2H"
-        if geo_type == "state":
-            feature_code = "ADM1H"
-            name = properties.get("STATENAM")
-            
+	geo_type = "county"	
+	if len(properties.get("NHGISNAM")) < 2 or properties.get("NHGISNAM") == None:
+	    feature_code == "ADM1H"
+            geo_type = "state"
+	    name = properties.get("STATENAM")
+
         if geo_type == "county":
-            
             name = properties.get("NHGISNAM")
-            #admin? 
+	    admin = [{"id": "", "feature_code": "ADM1H", "name": properties.get("NHGISNAM")}]
             
         if name == None:
             continue
             
         timeframe = {}
+	start_year = properties.get("START_DATE")
+	end_year = properties.get("END_DATE")
         
-        timeframe = {"start": year+"-01-01", "start_range":0,
-                     "end": str(int(year)+9)+"-12-31", "end_range":0}
+        timeframe = {"start": str(int(start_year))+"-01-01", "start_range":0,
+                     "end": str(int(end_year-1))+"-12-31", "end_range":0}
 
         source = properties
         updated = datetime.datetime.utcnow().replace(second=0, microsecond=0).isoformat()
         
-        uri = uri_name + "/" + feature["id"]
+        uri = uri_name + "/" + geo_type + "/" + feature["id"]
         
         place = {
             "name":name,
@@ -65,32 +69,42 @@ def extract_shapefile(shapefile, uri_name, simplify_tolerance, country_code, geo
             "uris":[uri],
             "relationships": [],
             "timeframe":timeframe,
-            "admin":[]
+            "admin":admin
         }
-
         dump.write(uri, place)
         
 
  
 if __name__ == "__main__":
-    shapefile_dir, dump_path = sys.argv[1:3]
+    shapefile, dump_path = sys.argv[1:3]
    
+    #simplify_tolerance = .01 # ~ 11km (.001 = 111m)
+    simplify_tolerance = None
+    uri_name = "http://nhgis.org/"
     
-    shapefile_list = glob.glob(shapefile_dir + "/*.shp")
+    dump_basename = os.path.basename(shapefile)
+    dump = Dump(dump_path + "/shapefile/"+ dump_basename + ".%04d.json.gz")
     
-    #print shapefile_list
-    for shapefile_path in shapefile_list:
-        dump_basename = os.path.basename(shapefile_path)
-        country_code,geo_type,year = dump_basename[:-4].split("_")
-        uri_name = "http://nhgis.org/"+country_code+"/"+geo_type+"/"+year
+    extract_shapefile(shapefile, uri_name, simplify_tolerance)
+    
+    dump.close()
 
-        simplify_tolerance = .001
+    
+   # #print shapefile_list
+   # for shapefile_path in shapefile_list:
+   #     dump_basename = os.path.basename(shapefile_path)
+   #     country_code,geo_type,year = dump_basename[:-4].split("_")
+   #     uri_name = "http://nhgis.org/"+country_code+"/"+geo_type+"/"+year
+
+    #    simplify_tolerance = .001
   
-        dump = Dump(dump_path + "/shapefile/"+  dump_basename.strip() + ".%04d.json.gz", max_rows=1000)
+    #    dump = Dump(dump_path + "/shapefile/"+  dump_basename.strip() + ".%04d.json.gz", max_rows=1000)
         
-        extract_shapefile(shapefile_path, uri_name, simplify_tolerance, country_code, geo_type, year)
+    #    extract_shapefile(shapefile_path, uri_name, simplify_tolerance, country_code, geo_type, year)
         
-        dump.close()
+    #    dump.close()
+
+#python nhgis.py nhgis_merged.shp nhgisdump
 
 #python nhgis.py  /home/tim/work/gazatteer/nhgis/reprojected nhgisdump
 
