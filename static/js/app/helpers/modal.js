76855bb6c9e1b07c8572a8cc2cb5eac14c6a2691
Sanjay B <b@pad.ma>
2013-04-19 22:43:42 +0530

    html / cruft for modals

--- static/js/app/helpers/modal.js
+++ static/js/app/helpers/modal.js
@@ -6,15 +6,27 @@ define(['require', 'jquery'], function(require, $) {
         };
 
         this.closeModal = function() {
-
+            var that = this;
+            require(['app/app', 'jquery'], function(app, $) {
+                app.modal.close();
+                $('#overlay').hide();
+                that.removeCloseHandler();
+            });
         };
 
         this.addCloseHandler = function() {
-
+            var that = this;
+            $('#overlay').bind('click', function() {
+                that.closeModal();
+            });
+            $('#lightBoxContent').bind('click', function(e) {
+                e.stopPropagation();
+            });
         };
 
         this.removeCloseHandler = function() {
-
+            $('#overlay').unbind('click');
+            $('#lightBoxContent').unbind('click');
         };
 
     }; 
