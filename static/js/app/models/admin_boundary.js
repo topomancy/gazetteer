de88bb618e193b47ec774e7ad4479ec35d311ebe
Tim Waters <chippy2005@gmail.com>
2013-05-11 12:46:07 +0100

    Fix bug where admin boundaries were not being displayed if there were no alternate names for that admin boundary

--- static/js/app/models/admin_boundary.js
+++ static/js/app/models/admin_boundary.js
@@ -3,6 +3,9 @@ define(['Backbone', 'app/settings'], function(Backbone, settings) {
     var AdminBoundary  = Backbone.Model.extend({
         initialize: function() {
             this.set('url', this.getURL());
+            if (typeof(this.get('alternate_names')) == 'undefined') {
+                this.set('alternate_names', []);
+            }
         },
 
         getURL: function() {
