34ef03f1192c02f222dcc03f8eaa67c0bbf99764
Schuyler Erle <schuyler@nocat.net>
2013-06-25 00:11:24 -0700

    Properly decode names, link up coordinates, etc.

--- etl/ranking/extract_coordinates.py
+++ etl/ranking/extract_coordinates.py
@@ -1,26 +1,27 @@
-import bz2, re, math, urllib, sys
+# run as: bzgrep georss geo_coordinates_en.nt.bz2 | python extract_coordinates.py | tee geo_features.txt
+import re, math, urllib, sys
 
 print >>sys.stderr, "extracting coordinates"
 # <http://dbpedia.org/resource/Alabama> <http://www.georss.org/georss/point> "33.0 -86.66666666666667"@en .
-coord_regex = re.compile(r'.*/resource/(\S+).*"(-?\d+\.\d+) (-?\d+\.\d+)"')
+coord_regex = re.compile(r'.*/resource/(\S+?)>.*"(-?\d+\.\d+) (-?\d+\.\d+)"')
 coords = {}
-for line in bz2.BZ2File("geo_coordinates_en.nt.bz2"):
+for line in sys.stdin:
     match = coord_regex.findall(line)
     if not match: continue
-    print >>sys.stderr, "\r", len(coords), 
     name, lat, lon = match[0]
-    coords[name] = (lat, lon)
+    coords[name] = (lon, lat)
+    print >>sys.stderr, "\r", len(coords),
 
 print >>sys.stderr, "\rextracting counts"
 counts = {}
 for line in file("geo_counts.txt"):
-    print >>sys.stderr, "\r", len(counts), 
     name, count = line.strip().split("\t")
-    counts[name] = int(count)
+    counts[name] = math.log(float(count))
+    print >>sys.stderr, "\r", len(counts),
 
 high_count = max(counts.values())
-for name, count in counts.items():
+for count, name in sorted((c, n) for n, c in counts.items()):
     count = float(count) / high_count
-    name = urllib.unquote(name).decode('utf8')
-    lat, lon = coords[name] if name in coords else ("0", "0")
-    print "\t".join(name, count, lon, lat)
+    lon, lat = coords[name] if name in coords else ("0", "0")
+    name = urllib.unquote(name.replace("_", " ")).decode('utf8')
+    print "\t".join(map(unicode, (name, count, lon, lat))).encode("utf-8")
